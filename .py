9fa4e429c024da1c774ba44852be685b33e9ae51
asafsalpe <asafs.alpe@gmail.com>
2023-12-12 13:28:08 +0200

    Update .py

--- .py
+++ .py
@@ -1,21 +1,22 @@
 from tkinter import*
 from PIL import ImageTk, Image
 import random
+from tkinter import messagebox
 gameWindow=Tk()
 gameWindow.title("Memory game")
 
-bgImg=ImageTk.PhotoImage(Image.open('skoda.jpg'))
+bgImg=ImageTk.PhotoImage(Image.open('bgImg.jpg'))
 
-btn0=Button(width=20, height=10, image=bgImg)
-btn1=Button(width=20, height=10)
-btn2=Button(width=20, height=10)
-btn3=Button(width=20, height=10)
-btn4=Button(width=20, height=10)
-btn5=Button(width=20, height=10)
-btn6=Button(width=20, height=10)
-btn7=Button(width=20, height=10)
-btn8=Button(width=20, height=10)
-btn9=Button(width=20, height=10)
+btn0=Button(width=150, height=150, image=bgImg)
+btn1=Button(width=150, height=150, image=bgImg)
+btn2=Button(width=150, height=150, image=bgImg)
+btn3=Button(width=150, height=150, image=bgImg)
+btn4=Button(width=150, height=150, image=bgImg)
+btn5=Button(width=150, height=150, image=bgImg)
+btn6=Button(width=150, height=150, image=bgImg)
+btn7=Button(width=150, height=150, image=bgImg)
+btn8=Button(width=150, height=150, image=bgImg)
+btn9=Button(width=150, height=150, image=bgImg)
 
 btn0.grid(row = 0, column = 0)
 btn1.grid(row = 0, column = 1)
