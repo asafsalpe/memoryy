a7de169c012566218553f0df070f6a9584b47378
asafsalpe <asafs.alpe@gmail.com>
2024-01-16 13:38:30 +0200

    Update .py

--- .py
+++ .py
@@ -1,43 +1,41 @@
-
-from cProfile import label
-from cgitb import reset
-from tkinter import*
+from tkinter import* #tkinter bibliotēka
 from PIL import ImageTk, Image
 import random
 from tkinter import messagebox
-gW=Tk()
+
+gW=Tk()#logs
 gW.title("Matching game")
 
-bgImg=ImageTk.PhotoImage(Image.open('bgImg.jpg'))
+bgImg=ImageTk.PhotoImage(Image.open('bgImg.jpg'))#fona attēls priekš visām kartītēm
 
-count=0
+count=0 #mainīgie
 correctAnswers=0
 answers=[]
 answer_dict={}
 answerCount=0
 
-def btnClick(btn,number):
+def btnClick(btn,number): #funkcija kartītes apgriešanai
     global count, correctAnswers, answers, answer_dict, answerCount
-    if btn['image']=='pyimage1'and count<2:
+    if btn['image']=='pyimage1'and count<2: #ja kartīte nav apgriesta un apgriestas mazāk par 2 kartītēm
         btn['image']=ImageList[number]
         count+=1
         answers.append(number)
         answer_dict[btn]=ImageList[number]
-    if len(answers)==2:
-        if ImageList[answers[0]]==ImageList[answers[1]]:
-            for key in answer_dict:
+    if len(answers)==2: #ja apgriestas 2 kartītes
+        if ImageList[answers[0]]==ImageList[answers[1]]: #vai kartītes vienādas
+            for key in answer_dict: #atspējot vienādās kartītes
                 key['state']=DISABLED
             correctAnswers+=2
-            if correctAnswers==2:
+            if correctAnswers==2:# ja ir apgriestas vienādas kartītes
                 messagebox.showinfo("Matching images", "Correct")
                 correctAnswers=0
                 answerCount+=1
-            if answerCount==5:
+            if answerCount==5: #ja pareizi atminētas visas 
                 messagebox.showinfo("Congrats!", "You win!!!")
                 reset()
-        else:
+        else: #ja apgrieztas dažādas kartītes
             messagebox.showinfo("Matching images","Incorrect")
-            for key in answer_dict:
+            for key in answer_dict: #apgriest kartīti atpakaļ
                 key['image']="pyimage1"
         count=0
         answers=[]
@@ -45,9 +43,9 @@ def btnClick(btn,number):
     
     return 0
 
-def reset():
+def reset(): #funkcija spēles sākšanai no jauna
     global count, correctAnswers, answers, answer_dict, answerCount
-    btn0.config(state=NORMAL)
+    btn0.config(state=NORMAL) #aktivizē visas pogas
     btn1.config(state=NORMAL)
     btn2.config(state=NORMAL)
     btn3.config(state=NORMAL)
@@ -58,7 +56,7 @@ def reset():
     btn8.config(state=NORMAL)
     btn9.config(state=NORMAL)
     
-    btn0["image"]="pyimage1"
+    btn0["image"]="pyimage1"#nomaina visām pogām bildi uz fona attēlu
     btn1["image"]="pyimage1"
     btn2["image"]="pyimage1"
     btn3["image"]="pyimage1"
@@ -69,7 +67,7 @@ def reset():
     btn8["image"]="pyimage1"
     btn9["image"]="pyimage1"
 
-    count=0
+    count=0 #mainīgos atiestata
     correctAnswers=0
     answers=[]
     answer_dict={}
@@ -78,10 +76,10 @@ def reset():
 
     return 0
 
-def info():
-    gWs=Toplevel()
+def info(): #funkcija spēles noteikumiem
+    gWs=Toplevel()#jauns logs
     gWs.title('Rules')
-    gWs.geometry('710x170')
+    gWs.geometry('710x170')#norāda loga parametrus
     desc=Label(gWs, text="Rules", font="Helvica 24 bold")
     desc.grid(row=0, column=0)
     desc=Label(gWs, text="The player turns over 2 cards", font="Helvica 14")
@@ -90,11 +88,11 @@ def info():
     desc.grid(row=2, column=0)
     desc=Label(gWs, text="If they do not match the cards are turned over again, and the player tries again", font="Helvica 14")
     desc.grid(row=3, column=0)
-    desc=Label(gWs, text="You must remember where you have seen the cards, so you know which ones to flip", font="Helvica 14")
+    desc=Label(gWs, text="The game ends when the player has matched all cards", font="Helvica 14")
     desc.grid(row=4, column=0)
     return 0 
 
-btn0=Button(width=150, height=150, image=bgImg, command=lambda:btnClick(btn0,0))
+btn0=Button(width=150, height=150, image=bgImg, command=lambda:btnClick(btn0,0))#pogas
 btn1=Button(width=150, height=150, image=bgImg, command=lambda:btnClick(btn1,1))
 btn2=Button(width=150, height=150, image=bgImg, command=lambda:btnClick(btn2,2))
 btn3=Button(width=150, height=150, image=bgImg, command=lambda:btnClick(btn3,3))
@@ -105,7 +103,7 @@ btn7=Button(width=150, height=150, image=bgImg, command=lambda:btnClick(btn7,7))
 btn8=Button(width=150, height=150, image=bgImg, command=lambda:btnClick(btn8,8))
 btn9=Button(width=150, height=150, image=bgImg, command=lambda:btnClick(btn9,9))
 
-btn0.grid(row = 0, column = 0)
+btn0.grid(row = 0, column = 0)#pogu novietojums
 btn1.grid(row = 0, column = 1)
 btn2.grid(row = 0, column = 2)
 btn3.grid(row = 0, column = 3)
@@ -116,25 +114,24 @@ btn7.grid(row = 1, column = 2)
 btn8.grid(row = 1, column = 3)
 btn9.grid(row = 1, column = 4)
 
-myImg1=ImageTk.PhotoImage(Image.open("bmw.jpg").resize((150,150)))
+myImg1=ImageTk.PhotoImage(Image.open("bmw.jpg").resize((150,150)))#bildes pogām
 myImg2=ImageTk.PhotoImage(Image.open("honda.jpg").resize((150,66)))
 myImg3=ImageTk.PhotoImage(Image.open("merc.jpg").resize((150,150)))
 myImg4=ImageTk.PhotoImage(Image.open("porsch.jpg").resize((150,58)))
 myImg5=ImageTk.PhotoImage(Image.open("skoda.jpg").resize((150,114)))
 
-ImageList=[myImg1,myImg1,myImg2,myImg2,myImg3,myImg3,myImg4,myImg4,myImg5,myImg5]
+ImageList=[myImg1,myImg1,myImg2,myImg2,myImg3,myImg3,myImg4,myImg4,myImg5,myImg5]#iedala katrai pogai bildi 
 random.shuffle(ImageList)
 
-mainMenu=Menu(gW)
+mainMenu=Menu(gW)#izvēlne
 gW.config(menu=mainMenu)
 
-options=Menu(mainMenu, tearoff=False)
+options=Menu(mainMenu, tearoff=False)#izvēlne
 mainMenu.add_cascade(label="Options", menu=options)
 
-options.add_command(label="New game", command=reset)
+options.add_command(label="New game", command=reset)# sākt no jauna un aizvērt
 options.add_command(label="Exit", command=gW.quit)
 
-mainMenu.add_command(label="About", command=info)
-
+mainMenu.add_command(label="About", command=info)#noteikumi
 
 gW.mainloop()
